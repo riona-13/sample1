7aa579d9f811d038d2a3ff5d815b51cc1b788531
riona-13 <55976440+riona-13@users.noreply.github.com>
2022-03-04 01:19:54 +0530

    Update C.sh

--- C.sh
+++ C.sh
@@ -1,11 +1,9 @@
 export PROJECT_ID=$(gcloud config get-value project)
 
 gsutil mb gs://$PROJECT_ID
-echo $PROJECT_ID
-gsutil cp *.csv gs://my-bucket
 
 
-export PROJECT_ID=$(gcloud config get-value project)
+
 gcloud sql instances create $PROJECT_ID \
 --database-version=MYSQL_5_7 \
 --region=us-central1
